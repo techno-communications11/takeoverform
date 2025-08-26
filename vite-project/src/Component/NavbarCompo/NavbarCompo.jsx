25510ed39b5bb5e1b15382ce70843a2de1a93de6
techno-communications11 <ptharun@techno-communications.com>
2025-08-27 01:05:18 +0530

    prod code

--- vite-project/src/Component/NavbarCompo/NavbarCompo.jsx
+++ vite-project/src/Component/NavbarCompo/NavbarCompo.jsx
@@ -7,7 +7,7 @@ function NavbarCompo() {
         <div className="row">
             <div className="col-md-12 d-flex align-items-center">
                 <img src="https://ticketing-systems-five.vercel.app/logo.webp" width={70} className='img-fluid' alt="" />
-                <span className='fw-semibold fs-4'>echnocummunication LLC</span>
+                <span className='fw-semibold fs-4'>Techno-Communication LLC</span>
             </div>
         </div>
     </div>
