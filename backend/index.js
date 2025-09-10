4f6dc796c0f7f1f525d1b0488323c42eb8dd40d8
techno-communications11 <ptharun@techno-communications.com>
2025-09-10 21:46:30 +0530

    cors updates

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,15 @@ connectionDB()
     .catch(err => console.log("DB NOT Connected",err));
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: "https://takeoverform.techno-communications.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use('/',routing)
 
 
